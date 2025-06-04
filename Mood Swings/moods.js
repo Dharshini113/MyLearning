732e17095483d164dc03d0eed9b4c33e37e5c0f6
Dharshini113 <145102888+Dharshini113@users.noreply.github.com>
2025-06-04 11:17:37 +0530

    Add files via upload

---
+++ Mood Swings/moods.js
@@ -0,0 +1,34 @@
+// let hap=document.getElementById('happy')
+// let sad=document.getElementById('sad')
+// console.log(hap)
+// console.log(sad)
+
+let photo = document.querySelector('img')
+console.log(photo)
+
+
+let b1=document.getElementsByTagName('button')[0]
+console.log(b1)
+
+let b2=document.getElementsByTagName('button')[1]
+console.log(b2)
+
+
+b1.addEventListener('click' ,function(){
+    photo.src="Happy.jpg"
+})
+
+b2.addEventListener('click' ,function(){
+    photo.src="Sad.jpg"
+})
+
+// function hemj(){
+//     alert("I am Happy")
+//     photo.src="Happy.jpg"
+// }
+
+// function semj(){
+//     alert("I am sad")
+//     photo.src="Sad.jpg"
+// }
+
